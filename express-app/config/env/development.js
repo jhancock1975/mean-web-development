da03470d00a86d82de1e18a90bd1e6ffd6702c3e
john <jhancock1975@gmail.com>
2016-07-30 13:03:43 -0400

    adding bower to app, and twitter oath example which does not work

--- express-app/config/env/development.js
+++ express-app/config/env/development.js
@@ -5,5 +5,10 @@ module.exports = {
     clientID: '323798774457174',
     clientSecret: 'c78a8d2dee3bf5f3e080d1d25d3bac1e',
     callbackURL: 'http://localhost:3001/oauth/facebook/callback'
+  },
+  twitter: {
+    clientID: 'eSjQ24SyIuOt5WuWKLX965RmF',
+    clientSecret: 'uVyj7CWFzeULQwT4Xe0wbIdqdgvBdKzyUhcnZzfNaaB7wEUS0V',
+    callbackURL: 'http://localhost:3001/oauth/twitter/callback'
   }
 };

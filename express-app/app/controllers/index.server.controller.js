e0fded027ffe21137e5a0487dfd95e6e3abdfb64
john <jhancock1975@gmail.com>
2016-08-01 07:46:56 -0400

    add user object from persistence layer, with a bug

--- express-app/app/controllers/index.server.controller.js
+++ express-app/app/controllers/index.server.controller.js
@@ -1,18 +1,6 @@
 exports.render = function(req, res) {
-
-  var lastVisit;
-
-  if (req.session.lastVisit) {
-    lastVisit = req.session.lastVisit;
-    console.log(req.session.lastVisit);
-  } else {
-    lastVisit = "never."
-  }
-
-  req.session.lastVisit = new Date();
-
-  res.render('index', {
+   res.render('index', {
     title: 'Hello World',
-    userFullName: req.user ? req.user.fullName : ''
+    user: JSON.stringify(req.user)
   });
 };

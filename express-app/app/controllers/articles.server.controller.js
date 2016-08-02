837685102d13bdb6e58ffa8cb71782a1a3effffe
John Hancock <jhancock1975@gmail.com>
2016-08-01 22:28:20 -0400

    add routes for crud module

--- express-app/app/controllers/articles.server.controller.js
+++ express-app/app/controllers/articles.server.controller.js
@@ -82,3 +82,12 @@ exports.delete = function(req, res) {
     }
   });
 };
+
+exports.hasAuthorization = function(req, res, next) {
+    if (req.article.creator.id !== req.user.id) {
+        return res.status(403).send({
+            message: 'User is not authorized'
+        });
+    }
+    next();
+};
